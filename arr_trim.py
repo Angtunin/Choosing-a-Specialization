40bb621646c405ac34170357b1633cd546b97b4e
AleksandrTunin <angtunin@mail.ru>
2023-09-28 16:22:39 +0800

    created function arrays_trim

--- arr_trim.py
+++ arr_trim.py
@@ -2,3 +2,10 @@ list1 = ['hello', '2', 'world', ',!__@', '222']
 list2 = ['13444', '155954', '123', '2']
 text = 'To add a new remote, use the git remote add command on the terminal, in the directory your'
 list3 = text.split()
+def array_trim(a):
+    new_list = []
+    for i in range(len(a)):
+        if len(a[i]) <= 3:
+            new_list.append(a[i])
+    a = new_list
+    return a
