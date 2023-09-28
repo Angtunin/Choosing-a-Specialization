c00d807826f6f84e5b14e63537834c5ee92032f4
AleksandrTunin <angtunin@mail.ru>
2023-09-28 15:32:03 +0800

    create file arr_trim.py

---
+++ arr_trim.py
@@ -0,0 +1,4 @@
+list1 = ['hello', '2', 'world', ',!__@']
+list2 = ['13444', '155954', '123', '2']
+text = 'To add a new remote, use the git remote add command on the terminal, in the directory your'
+list3 = text.split()
